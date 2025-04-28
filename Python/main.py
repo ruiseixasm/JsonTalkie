71d6253b27226b9917cef394ee54372f93b09901
Rui Monteiro <ruiseixas@yahoo.com>
2025-04-28 04:06:41 +0100

    Improved composition

--- Python/main.py
+++ Python/main.py
@@ -2,31 +2,33 @@ from typing import Dict, Any
 import time
 
 from broadcast_socket_udp import *
+from json_talkie import *
 from walkie_device import *
 
 if __name__ == "__main__":
 
     broadcast_socket: BroadcastSocket = BroadcastSocket_UDP()
-    json_device: WalkieDevice = WalkieDevice(broadcast_socket)
+    json_talkie: JsonTalkie = JsonTalkie(broadcast_socket)
+    walkie_device: WalkieDevice = WalkieDevice(json_talkie)
 
     # Start listening (opens socket)
-    if not json_device.start():
+    if not walkie_device.start():
         print("Failed to start device!")
         exit(1)
     
-    print(f"Device {json_device._name} running. Press Ctrl+C to stop.")
+    print(f"Device {walkie_device._name} running. Press Ctrl+C to stop.")
     
     try:
         # Main loop
         while True:
-            json_device.send_json(
-                {'command': 'call', 'function': 'buzz', 'from': json_device._name, 'to': 'Buzzer'}
+            walkie_device.talk(
+                {'command': 'call', 'function': 'buzz', 'from': walkie_device._name, 'to': 'Buzzer'}
             )
             time.sleep(3)  # Send ping every 3 seconds
             
     except KeyboardInterrupt:
         print("\nShutting down...")
     finally:
-        json_device.stop()  # Ensures socket cleanup
+        walkie_device.stop()  # Ensures socket cleanup
 
 
