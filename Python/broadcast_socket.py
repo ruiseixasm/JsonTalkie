5618c0347068718d67c803bda68e51609c9d7393
Rui Monteiro <ruiseixas@yahoo.com>
2025-04-29 22:29:18 +0100

    Clean code

--- Python/broadcast_socket.py
+++ Python/broadcast_socket.py
@@ -3,9 +3,7 @@ from typing import Optional, Tuple
 class BroadcastSocket:
     """UDP broadcast socket with explicit lifecycle control."""
     
-    def __init__(self, port: int = 5005, broadcast_addr: str = '255.255.255.255'):
-        self._port = port
-        self._broadcast_addr = broadcast_addr
+    def __init__(self, *parameters):
         self._socket = None  # Not initialized until open()
     
     def open(self) -> bool:
@@ -14,8 +12,7 @@ class BroadcastSocket:
     
     def close(self):
         """Release socket resources."""
-        if self._socket:
-            self._socket = None
+        return True
     
     def send(self, data: bytes) -> bool:
         """Broadcast data if socket is active."""
