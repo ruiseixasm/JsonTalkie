e6c3ee1fcefa76145d0d5de356d0b45b05fc9933
Rui Monteiro <ruiseixas@yahoo.com>
2025-04-29 23:27:41 +0100

    Arduino Compiling code

--- Arduino/JsonTalkie/BroadcastSocket_UDP.h
+++ Arduino/JsonTalkie/BroadcastSocket_UDP.h
@@ -2,30 +2,20 @@
 #define BROADCAST_SOCKET_UDP_H
 
 #include "BroadcastSocket.h"
-#include <Ethernet.h>      // For EthernetUDP and IPAddress
-#include <EthernetUdp.h>   // For UDP functionality
+#include <Ethernet.h>
+#include <EthernetUdp.h>
 
 class BroadcastSocket_UDP : public BroadcastSocket {
 public:
-    /**
-     * @param port UDP port to use (default 5005)
-     */
-    explicit BroadcastSocket_UDP(uint16_t port = 5005);
+    explicit BroadcastSocket_UDP(uint16_t port = 5005); // Port set here
     
-    bool begin() override;
+    bool begin() override; // No parameters
     void end() override;
     bool write(const uint8_t* data, size_t length) override;
     bool available() override;
     size_t read(uint8_t* buffer, size_t size) override;
     
-    /**
-     * Gets the sender IP of the last received packet
-     */
     IPAddress getSenderIP() const { return _remoteIP; }
-    
-    /**
-     * Gets the sender port of the last received packet
-     */
     uint16_t getSenderPort() const { return _remotePort; }
 
 private:
