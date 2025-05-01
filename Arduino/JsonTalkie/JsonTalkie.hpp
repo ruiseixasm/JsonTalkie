f47bd9dfd48804383b9b59218b93aecdf0fd764f
Rui Monteiro <ruiseixas@yahoo.com>
2025-05-01 18:03:21 +0100

    Improved JsonTalkie header file

--- Arduino/JsonTalkie/JsonTalkie.hpp
+++ Arduino/JsonTalkie/JsonTalkie.hpp
@@ -19,6 +19,90 @@ https://github.com/ruiseixasm/JsonTalkie
 #include <ArduinoJson.h>    // Install ArduinoJson Library
 
 
+// HELPER METHODS
+
+// Place this ABOVE your Talker class definition
+char* floatToStr(float val, uint8_t decimals = 2) {
+    static char buffer[16]; // Holds "-327.00" (large enough for most cases)
+    dtostrf(val, 0, decimals, buffer); // Works on ALL Arduino boards
+    return buffer;
+}
+
+
+// MANIFESTO PROTOTYPING
+
+struct Talker {
+    const char* name;      // Name of the Device (Talker)
+    const char* desc;      // Description of the Device
+};
+
+struct Run {
+    const char* name;      // "buzz", "print", etc.
+    const char* desc;      // Description
+    const char* (*function)();  // Function pointer (no args)
+};
+
+struct Set {
+    const char* name;      // "buzz", "print", etc.
+    const char* desc;      // Description
+    const char* (*function)(const char*);  // Function pointer (const char*)
+};
+
+struct Get {
+    const char* name;      // "buzz", "print", etc.
+    const char* desc;      // Description
+    const char* (*function)();  // Function pointer (no args)
+};
+
+// Structure Definition
+struct Manifesto {
+
+    static const Talker talker;         // Declaration only
+    static const Run runCommands[];     // Declaration only
+    static const size_t runSize;        // Declaration only
+    static const Set setCommands[];
+    static const size_t setSize;        // Declaration only
+    static const Get getCommands[];
+    static const size_t getSize;        // Declaration only
+    static bool (*echo)(StaticJsonDocument<256>*, const char*);
+};
+
+// Triggering methods definitions
+
+const Talker* talk() {
+    return &Manifesto::talker;
+}
+
+const char* run(const char* cmd) {
+    for (int index = 0; index < Manifesto::runSize; ++index) {
+        if (strcmp(cmd, Manifesto::runCommands[index].name) == 0) {
+            return (Manifesto::runCommands[index].function)();  // Call the function
+        }
+    }
+    return "Command not found";
+}
+
+const char* set(const char* cmd, const char* value) {
+    for (int index = 0; index < Manifesto::runSize; ++index) {
+        if (strcmp(cmd, Manifesto::setCommands[index].name) == 0) {
+            return (Manifesto::setCommands[index].function)(value);  // Call the function
+        }
+    }
+    return "Command not found";
+}
+
+const char* get(const char* cmd) {
+    for (int index = 0; index < Manifesto::runSize; ++index) {
+        if (strcmp(cmd, Manifesto::getCommands[index].name) == 0) {
+            return (Manifesto::getCommands[index].function)();  // Call the function
+        }
+    }
+    return "Command not found";
+}
+
+
+// JSONTALKIE DEFINITIONS
+
 class JsonTalkie {
 private:
     BroadcastSocket* _socket;
