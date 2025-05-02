83f1b1f647fe39d1ab7b9f6172d9b8469519c26b
Rui Monteiro <ruiseixas@yahoo.com>
2025-05-02 21:38:45 +0100

    Added BroadcastSocket implementations

---
+++ Arduino/JsonTalkie/sockets/BroadcastSocket_EtherCard.hpp
@@ -0,0 +1,24 @@
+/*
+JsonTalkie - Json Talkie is intended for direct IoT communication.
+Original Copyright (c) 2025 Rui Seixas Monteiro. All right reserved.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+https://github.com/ruiseixasm/JsonTalkie
+*/
+#ifndef BROADCAST_SOCKET_ETHERCARD_HPP
+#define BROADCAST_SOCKET_ETHERCARD_HPP
+
+#include "../BroadcastSocket.hpp"
+#include <Arduino.h>    // Needed for Serial given that Arduino IDE only includes Serial in .ino files!
+#include <EtherCard.h>
+
+
+
+
+#endif // BROADCAST_SOCKET_ETHERCARD_HPP
