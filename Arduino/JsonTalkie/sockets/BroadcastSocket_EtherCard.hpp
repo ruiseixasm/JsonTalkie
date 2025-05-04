4693fc72140fae9521c5cce1dab57216f07fbc17
Rui Monteiro <ruiseixas@yahoo.com>
2025-05-04 20:42:49 +0100

    Added BroadcastSocket header file

--- Arduino/JsonTalkie/sockets/BroadcastSocket_EtherCard.hpp
+++ Arduino/JsonTalkie/sockets/BroadcastSocket_EtherCard.hpp
@@ -20,7 +20,71 @@ https://github.com/ruiseixasm/JsonTalkie
 
 
 class BroadcastSocket_EtherCard : public BroadcastSocket {
+private:
+
+    static BroadcastSocket_EtherCard* _instance;
+    uint16_t _port;
+    uint8_t* _mac;
+    uint8_t _csPin;
+    uint16_t _remotePort;
+    uint8_t _remoteIp[4];
+    uint8_t _recvBuffer[UDP_BUFFER_SIZE];
+    size_t _recvLength;
     
+    void handlePacket(uint16_t dest_port, uint8_t* src_ip, uint16_t src_port, const char* data, uint16_t len) {
+        if (len > UDP_BUFFER_SIZE) return;
+        memcpy(_recvBuffer, data, len);
+        _recvLength = len;
+        memcpy(_remoteIp, src_ip, 4);
+        _remotePort = src_port;
+    }
+
+    static void udpCallback(uint16_t dest_port, uint8_t* src_ip, uint16_t src_port, const char* data, uint16_t len) {
+    if (_instance) {
+        _instance->handlePacket(dest_port, src_ip, src_port, data, len);
+    }
+}
+public:
+    BroadcastSocket_EtherCard(uint16_t port, uint8_t* mac, uint8_t csPin = 8) 
+        : _port(port), _mac(mac), _csPin(csPin), _remotePort(0), _recvLength(0) {
+            memset(_remoteIp, 0, sizeof(_remoteIp));
+            memset(_recvBuffer, 0, sizeof(_recvBuffer));
+            _instance = this;
+    }
+    
+    bool begin() override {
+        if (ether.begin(ETHER_BUFFER_SIZE, _mac, _csPin)) {
+            ether.udpServerListenOnPort(udpCallback, _port);
+            return true;
+        }
+        return false;
+    }
+
+    void end() override {
+        _instance = nullptr;
+    }
+
+    bool write(const uint8_t* data, size_t length) override {
+        if (length > UDP_BUFFER_SIZE) return false;
+        uint8_t broadcastIp[] = {255,255,255,255};
+        ether.sendUdp((char*)data, length, _port, broadcastIp, _port);
+        return true;
+    }
+
+    bool available() override {
+        return _recvLength > 0;
+    }
+
+    size_t read(uint8_t* buffer, size_t size) override {
+        if (!buffer || size == 0) return 0;
+        size_t toCopy = min(size, _recvLength);
+        memcpy(buffer, _recvBuffer, toCopy);
+        _recvLength = 0;
+        return toCopy;
+    }
+    
+    const uint8_t* getSenderIP() const { return _remoteIp; }
+    uint16_t getSenderPort() const { return _remotePort; }
 
 };  
 
