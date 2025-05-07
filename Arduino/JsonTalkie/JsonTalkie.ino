41e8420c332858b888a7d0b5c44f3fe431b218f3
Rui Monteiro <ruiseixas@yahoo.com>
2025-05-07 13:38:23 +0100

    Using PROGMEM

--- Arduino/JsonTalkie/JsonTalkie.ino
+++ Arduino/JsonTalkie/JsonTalkie.ino
@@ -93,6 +93,7 @@ void setup() {
     
     delay(2000);    // Just to give some time to Serial
 
+    Serial.println(F("Opening the Socket..."));
     // MAC and CS pin in constructor
     // SS is a macro variable normally equal to 10
     if (!broadcast_socket.open(mymac, SS, 5005)) {
