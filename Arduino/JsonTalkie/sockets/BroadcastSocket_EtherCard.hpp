0ce281ed2cd1e06f1b6bbe6561a99f334803071b
Rui Monteiro <ruiseixas@yahoo.com>
2025-05-07 14:54:44 +0100

    Working message exchange

--- Arduino/JsonTalkie/sockets/BroadcastSocket_EtherCard.hpp
+++ Arduino/JsonTalkie/sockets/BroadcastSocket_EtherCard.hpp
@@ -21,7 +21,7 @@ https://github.com/ruiseixasm/JsonTalkie
 
 #define UDP_BUFFER_SIZE 128
 #define ETHER_BUFFER_SIZE 256   // Typical size for ENC28J60 is 500, but for UDP only 256 is good enough
-#define BROADCAST_SOCKET_DEBUG
+// #define BROADCAST_SOCKET_DEBUG
 
 uint8_t Ethernet::buffer[ETHER_BUFFER_SIZE] = {0};  // Now reported! (Essential for EtherCard)
 
