f939d544c14aed1feb1f96f1756521796a99e788
Rui Monteiro <ruiseixas@yahoo.com>
2025-05-15 18:58:15 +0100

    Improved code

--- Arduino/JsonTalkie/JsonTalkie.ino
+++ Arduino/JsonTalkie/JsonTalkie.ino
@@ -11,12 +11,12 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 Lesser General Public License for more details.
 https://github.com/ruiseixasm/JsonTalkie
 */
-// #include "sockets/BroadcastSocket_EtherCard.hpp"
-#include "dummies/BroadcastSocket_Dummy.hpp"
+#include "sockets/BroadcastSocket_EtherCard.hpp"
+// #include "dummies/BroadcastSocket_Dummy.hpp"
 #include "JsonTalkie.hpp"
 
-// auto& broadcast_socket = BroadcastSocket_EtherCard::instance();
-auto& broadcast_socket = BroadcastSocket_Dummy::instance();
+auto& broadcast_socket = BroadcastSocket_EtherCard::instance();
+// auto& broadcast_socket = BroadcastSocket_Dummy::instance();
 JsonTalkie json_talkie;
 
 
@@ -88,19 +88,19 @@ void setup() {
     // Saving string in PROGMEM (flash) to save RAM memory
     Serial.println("\n\nOpening the Socket...");
     
-    // // MAC and CS pin in constructor
-    // // SS is a macro variable normally equal to 10
-    // if (!ether.begin(ETHERNET_BUFFER_SIZE, mac, SS)) {
-    //     Serial.println("Failed to access ENC28J60");
-    //     while (1);
-    // }
-    // // Set static IP (disable DHCP)
-    // if (!ether.staticSetup(my_ip, gw_ip, dns_ip, mask)) {
-    //     Serial.println("Failed to access ENC28J60");
-    //     while (1);
-    // }
-    // // Makes sure it allows broadcast
-    // ether.enableBroadcast();
+    // MAC and CS pin in constructor
+    // SS is a macro variable normally equal to 10
+    if (!ether.begin(ETHERNET_BUFFER_SIZE, mac, SS)) {
+        Serial.println("Failed to access ENC28J60");
+        while (1);
+    }
+    // Set static IP (disable DHCP)
+    if (!ether.staticSetup(my_ip, gw_ip, dns_ip, mask)) {
+        Serial.println("Failed to access ENC28J60");
+        while (1);
+    }
+    // Makes sure it allows broadcast
+    ether.enableBroadcast();
 
     // By default is already 5005
     broadcast_socket.set_port(5005);
