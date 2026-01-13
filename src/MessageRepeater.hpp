1787991d7cfdeab1efa27c6cf62476c3a116d587
Rui Monteiro <ruiseixas@yahoo.com>
2026-01-13 13:27:18 +0000

    Small correction in the uplinked sockets

--- src/MessageRepeater.hpp
+++ src/MessageRepeater.hpp
@@ -287,12 +287,6 @@ public:
 				// Sockets ONLY manipulate the checksum ('c')
 				_downlinked_sockets[socket_j]->_finishTransmission(message);
 			}
-			for (uint8_t socket_j = 0; socket_j < _uplinked_sockets_count; ++socket_j) {
-				if (_uplinked_sockets[socket_j]->getLinkType() == LinkType::TALKIE_LT_UP_BRIDGED) {
-					// Sockets ONLY manipulate the checksum ('c')
-					_uplinked_sockets[socket_j]->_finishTransmission(message);
-				}
-			}
 			
 			#ifdef MESSAGE_DEBUG_TIMING
 			Serial.print(" | ");
