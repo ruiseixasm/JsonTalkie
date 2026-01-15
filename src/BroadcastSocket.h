0bbef777772280804fd466c738e62afeb1d1b2b0
Rui Monteiro <ruiseixas@yahoo.com>
2026-01-15 09:28:39 +0000

    Compiles WiFi

--- src/BroadcastSocket.h
+++ src/BroadcastSocket.h
@@ -113,7 +113,7 @@ protected:
 
 		if (json_message._validate_json() && json_message._process_checksum() &&
 			json_message.get_from_name(_from_talker.name) &&
-			json_message.get_broadcast_value(_from_talker.broadcast)) {
+			json_message.get_broadcast_value(&_from_talker.broadcast)) {
 
 			_showMessage(json_message);
 			
