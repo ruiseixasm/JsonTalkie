87f09793b0dcfaadb7a39641d98effb72083274e
Rui Monteiro <ruiseixas@yahoo.com>
2026-01-25 23:20:51 +0000

    Recovery made safer

--- src/BroadcastSocket.h
+++ src/BroadcastSocket.h
@@ -415,16 +415,47 @@ protected:
 						Serial.println((int)corruption_type);
 						#endif
 			
-						// a match from with a single char difference top and one of the 'i' or 'c' matching too
-						if ((json_message.is_from_name(_corrupted_message.from_name) && message_length == _corrupted_message.length
-							&& (message_identity == _corrupted_message.identity || message_checksum == _corrupted_message.checksum))
-							|| (message_identity == _corrupted_message.identity && message_checksum == _corrupted_message.checksum)) {
+						switch (_corrupted_message.corruption_type) {
 
-							++_recoveries_count;	// It is a recovered message (+1)
-							_corrupted_message.active = false;
-						} else {
-							// Not for this Socket, let the Repeater send to other Sockets
-							json_message.replace_key('m', 'M');	// Replaces the tag for other Socket
+							case TALKIE_CT_IDENTITY:
+								// a match from with a single char difference top and one of the 'i' or 'c' matching too
+								if (json_message.is_from_name(_corrupted_message.from_name) && message_length == _corrupted_message.length
+									&& message_checksum == _corrupted_message.checksum) {
+
+									++_recoveries_count;	// It is a recovered message (+1)
+									_corrupted_message.active = false;
+								} else {
+									// Not for this Socket, let the Repeater send to other Sockets
+									json_message.replace_key('m', 'M');	// Replaces the tag for other Socket
+								}
+							break;
+
+							case TALKIE_CT_CHECKSUM:
+								// a match from with a single char difference top and one of the 'i' or 'c' matching too
+								if (json_message.is_from_name(_corrupted_message.from_name) && message_length == _corrupted_message.length
+									&& message_identity == _corrupted_message.identity) {
+
+									++_recoveries_count;	// It is a recovered message (+1)
+									_corrupted_message.active = false;
+								} else {
+									// Not for this Socket, let the Repeater send to other Sockets
+									json_message.replace_key('m', 'M');	// Replaces the tag for other Socket
+								}
+							break;
+
+							default:
+								// a match from with a single char difference top and one of the 'i' or 'c' matching too
+								if ((json_message.is_from_name(_corrupted_message.from_name) && message_length == _corrupted_message.length
+									&& (message_identity == _corrupted_message.identity || message_checksum == _corrupted_message.checksum))
+									|| (message_identity == _corrupted_message.identity && message_checksum == _corrupted_message.checksum)) {
+
+									++_recoveries_count;	// It is a recovered message (+1)
+									_corrupted_message.active = false;
+								} else {
+									// Not for this Socket, let the Repeater send to other Sockets
+									json_message.replace_key('m', 'M');	// Replaces the tag for other Socket
+								}
+							break;
 						}
 
 						#if defined(BROADCASTSOCKET_DEBUG_CHECKSUM_ALL)
