3a9198b5468024037beda64aeb7b36d5ab60e74c
Rui Monteiro <ruiseixas@yahoo.com>
2026-02-01 23:33:43 +0000

    Added the sketch ESP_Arduino

--- sockets/SPI/ESP/S_Broadcast_SPI_2xESP_Slave.hpp
+++ sockets/SPI/ESP/S_Broadcast_SPI_2xESP_Slave.hpp
@@ -11,8 +11,8 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 Lesser General Public License for more details.
 https://github.com/ruiseixasm/JsonTalkie
 */
-#ifndef BROADCAST_SPI_ESP_SLAVE_HPP
-#define BROADCAST_SPI_ESP_SLAVE_HPP
+#ifndef BROADCAST_SPI_ESP2X_SLAVE_HPP
+#define BROADCAST_SPI_ESP2X_SLAVE_HPP
 
 
 #include <BroadcastSocket.h>
@@ -297,4 +297,4 @@ public:
 };
 
 
-#endif // BROADCAST_SPI_ESP_SLAVE_HPP
+#endif // BROADCAST_SPI_ESP2X_SLAVE_HPP
