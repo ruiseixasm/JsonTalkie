7b6faae124d781803f8032160e9ca575554b993f
Rui Monteiro <ruiseixas@yahoo.com>
2026-02-05 18:57:48 +0000

    GIT completely out of hand

--- extras/Sketches/ESP32TestPair_Broadcast_SPI_2xESP_Master_1byte/S_Broadcast_SPI_2xESP_Master.hpp
+++ extras/Sketches/ESP32TestPair_Broadcast_SPI_2xESP_Master_1byte/S_Broadcast_SPI_2xESP_Master.hpp
@@ -11,26 +11,28 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 Lesser General Public License for more details.
 https://github.com/ruiseixasm/JsonTalkie
 */
-#ifndef BROADCAST_SPI_ESP2X_SLAVE_HPP
-#define BROADCAST_SPI_ESP2X_SLAVE_HPP
+#ifndef BROADCAST_SPI_ESP2X_MASTER_HPP
+#define BROADCAST_SPI_ESP2X_MASTER_HPP
 
 
 #include <BroadcastSocket.h>
-extern "C" {
-    #include "driver/spi_slave.h"
-}
+#include "driver/spi_master.h"
 
 
 // #define BROADCAST_SPI_DEBUG
 // #define BROADCAST_SPI_DEBUG_TIMING
 
+// Broadcast SPI is fire and forget, so, it is needed to give some time to the Slaves catch up with the next send from the Master
+#define broadcast_time_slot_us 500	// Gives some time to all Slaves to process the received broadcast before a next one
+#define beacon_time_slot_us 100		// Avoids too frequent beacons (used to collect data from the SPI Slaves)
 
-class S_Broadcast_SPI_2xESP_Slave : public BroadcastSocket {
+
+class S_Broadcast_SPI_2xESP_Master : public BroadcastSocket {
 public:
 
 	// The Socket class description shouldn't be greater than 35 chars
 	// {"m":7,"f":"","s":3,"b":1,"t":"","i":58485,"0":1,"1":"","2":11,"c":11266} <-- 128 - (73 + 2*10) = 35
-    const char* class_description() const override { return "Broadcast_SPI_2xESP_Slave"; }
+    const char* class_description() const override { return "Broadcast_SPI_2xESP_Master"; }
 
 
 	#ifdef BROADCAST_SPI_DEBUG_TIMING
@@ -39,32 +41,26 @@ public:
 
 protected:
 
-	enum SpiState {
-		WAIT_STATUS,
-		TX_PAYLOAD,
-		RX_PAYLOAD
-	};
-
 	bool _initiated = false;
+	
+    const int* _spi_cs_pins;
+    const uint8_t _ss_pins_count;
 	const spi_host_device_t _host;
-
-	// Two TX buffers are required because the SPI slave driver uses DMA and does not copy TX data.
-	// DMA descriptors are built from the tx_buffer pointer at queue time and may be reused.
-	// Alternating the buffer address guarantees a new DMA descriptor and prevents the previous
-	// payload from being transmitted again.
-	uint8_t _tx_status_byte[2] __attribute__((aligned(4))) = {0};
-	uint8_t _tx_status_index = 0;
+	
+	spi_device_handle_t _spi;
+	uint8_t _tx_status_byte __attribute__((aligned(4))) = 0;
 	uint8_t _rx_status_byte __attribute__((aligned(4))) = 0;
-	uint8_t _tx_payload_buffer[TALKIE_BUFFER_SIZE] __attribute__((aligned(4))) = {0};
-	uint8_t _rx_payload_buffer[TALKIE_BUFFER_SIZE] __attribute__((aligned(4))) = {0};
+	uint8_t _payload_buffer[TALKIE_BUFFER_SIZE] __attribute__((aligned(4))) = {0};
 
-	SpiState _spi_state = WAIT_STATUS;
-	spi_slave_transaction_t _data_trans __attribute__((aligned(4)));
-	size_t _payload_length = 0;
-	uint8_t _stacked_transmissions = 0;
+	bool _in_broadcast_slot = false;
+	uint32_t _broadcast_time_us = 0;
+	// Too many SPI sends to the Slaves asking if there is something to send will overload them, so, a timeout is needed
+	uint32_t _last_beacon_time_us = 0;
+
 
     // Constructor
-    S_Broadcast_SPI_2xESP_Slave(spi_host_device_t host) : BroadcastSocket(), _host(host) {
+    S_Broadcast_SPI_2xESP_Master(const int* ss_pins, uint8_t ss_pins_count, spi_host_device_t host)
+		: BroadcastSocket(), _spi_cs_pins(ss_pins), _ss_pins_count(ss_pins_count), _host(host) {
             
 		_max_delay_ms = 0;  // SPI is sequencial, no need to control out of order packages
 	}
@@ -73,94 +69,47 @@ protected:
     // Socket processing is always Half-Duplex because there is just one buffer to receive and other to send
     void _receive() override {
 
-		if (_initiated) {
+		// Sends once per pin, avoids getting stuck in processing many pins
+		static uint8_t actual_pin_index = 0;
+
+		if (_in_broadcast_slot && micros() - _broadcast_time_us > broadcast_time_slot_us) {
+			_in_broadcast_slot = false;
+		}
+
+		// Master gives priority to broadcast send, NOT to receive, so, it respects the broadcast time slot
+		if (!_in_broadcast_slot && _initiated) {
 			
-			// Checks if the queued element was consumed and needs to be processed, otherwise, return
-    		spi_slave_transaction_t *ret = nullptr;
-			if (spi_slave_get_trans_result(_host, &ret, 0) != ESP_OK) {
-				return;
-			}
+			// Too many SPI sends to the Slaves asking if there is something to send will overload them, so, a timeout is needed
+			if (micros() - _last_beacon_time_us > beacon_time_slot_us) {
+				_last_beacon_time_us = micros();	// Avoid calling the beacon right away
 
-			const size_t tx_length = (size_t)_tx_status_byte[_tx_status_index];
-			const size_t rx_length = (size_t)_rx_status_byte;
+				#ifdef BROADCAST_SPI_DEBUG_TIMING
+				_reference_time = millis();
+				#endif
+			
+				// Arms the receiving
+				size_t payload_length = sendBeacon(_spi_cs_pins[actual_pin_index]);
+				if (payload_length > 0) {
 
-
-			/* === SPI "ISR" === */
-
-			switch (_spi_state) {
-
-				case WAIT_STATUS:
-				{
-					if (rx_length == 0) {	// It's a beacon
-
-						if (tx_length > 0) {
-							queue_tx(tx_length);
-							return;
-						}
-
-					} else if (rx_length < TALKIE_BUFFER_SIZE + 1) {
-
-						if (rx_length > 0) {
-							queue_rx(rx_length);
-							return;
-						}
-					}
-				}
-				break;
-				
-				case TX_PAYLOAD:
-				{
-					if (tx_length > 0) {
-
-						#ifdef BROADCAST_SPI_DEBUG
-							Serial.printf("Sent %u bytes: ", tx_length);
-							for (uint8_t i = 0; i < tx_length; i++) {
-								char c = _tx_payload_buffer[i];
-								if (c >= 32 && c <= 126) Serial.print(c);
-								else Serial.printf("[%02X]", c);
-							}
-							Serial.println();
-						#endif
-
-						_payload_length = 0;	// payload was sent
-						_tx_status_index ^= 1;	// Rotate status Byte
-					}
-				}
-				break;
-				
-				case RX_PAYLOAD:
-				{
-					
+					receivePayload(_spi_cs_pins[actual_pin_index], payload_length);
 					#ifdef BROADCAST_SPI_DEBUG
-						Serial.printf("Received %u bytes: ", rx_length);
-						for (uint8_t i = 0; i < rx_length; i++) {
-							char c = _rx_payload_buffer[i];
-							if (c >= 32 && c <= 126) Serial.print(c);
-							else Serial.printf("[%02X]", c);
+						Serial.printf("[From Beacon to pin %d] Slave: 0x%02X Beacon=1 L=%d\n",
+							_spi_cs_pins[actual_pin_index], 0b10000000 | payload_length, payload_length);
+						Serial.print("[From Slave] Received: ");
+						for (int i = 0; i < payload_length; i++) {
+							Serial.print((char)_payload_buffer[i]);
 						}
 						Serial.println();
 					#endif
-
-					if (_stacked_transmissions < 3) {
-						
-						_rx_status_byte = 0;	// Makes suer the receiving by isn't kept as rx_length
-						queue_status();	// Safe to star before given tha tit's a different buffer (just a byte)
-						JsonMessage new_message(
-							reinterpret_cast<const char*>( _rx_payload_buffer ), rx_length
-						);
-						
-						_stacked_transmissions++;
-						_startTransmission(new_message);
-						_stacked_transmissions--;
-
-						return;	// Avoids the queue_status() call bellow (no repetition)
-					}
+					
+					// No receiving while a send is pending, so, no _json_message corruption is possible
+					JsonMessage new_message(reinterpret_cast<const char*>( _payload_buffer ), payload_length);
+					_startTransmission(new_message);
 				}
-				break;
+				actual_pin_index = (actual_pin_index + 1) % _ss_pins_count;
 			}
-			queue_status();
 		}
-	}
+    }
 
     
     // Socket processing is always Half-Duplex because there is just one buffer to receive and other to send
@@ -168,98 +117,150 @@ protected:
 
 		if (_initiated) {
 			
-			const uint16_t start_waiting = (uint16_t)millis();
-			while (_payload_length > 0) {
+			#ifdef BROADCAST_SPI_DEBUG_TIMING
+				Serial.print("\n\tsend: ");
+				_reference_time = millis();
+			#endif
 
-				if (_stacked_transmissions < 3) {
+			#ifdef BROADCAST_SPI_DEBUG_TIMING
+			Serial.print(" | ");
+			Serial.print(millis() - _reference_time);
+			#endif
 
-					_receive();	// keeps processing pending messages, mainly the ones pooled to be sent
-					if ((uint16_t)millis() - start_waiting > 1 * 1000) {
+			size_t length = json_message.serialize_json(
+				reinterpret_cast<char*>( _payload_buffer ),
+				TALKIE_BUFFER_SIZE
+			);
+			
+			if (length > 0) {
 
-						#ifdef BROADCAST_SPI_DEBUG
-							Serial.println(F("\t_unlockSendingBuffer: NOT available sending buffer"));
-						#endif
+				while (_in_broadcast_slot) {	// Avoids too many sends too close in time
+					// Broadcast has priority over receiving, so, no beacons are sent during broadcast time slot!
+					if (micros() - _broadcast_time_us > broadcast_time_slot_us) _in_broadcast_slot = false;
+				}
 
-						return false;
-					}
-
-				// If there is already 3 messages staked, then, start dropping sends
-				// Receive HAS priority over send!
-				} else {	// Start dropping
-					return false;
+				#ifdef BROADCAST_SPI_DEBUG
+				Serial.print(F("\t\t\t\t\tsend1: Sent message: "));
+				Serial.write(json_message._read_buffer(), json_message.get_length());
+				Serial.print(F("\n\t\t\t\t\tsend2: Sent length: "));
+				Serial.println(json_message.get_length());
+				#endif
+			
+				if (broadcastLength(_spi_cs_pins, _ss_pins_count, length) && broadcastPayload(_spi_cs_pins, _ss_pins_count, length)) {
+					_broadcast_time_us = micros();	// send time spacing applies after the sending (avoids bursting)
+					_in_broadcast_slot = true;
+					return true;
 				}
 			}
-			// Both, _tx_buffer and _payload_length, set at the same time
-			char* payload_buffer = reinterpret_cast<char*>( _tx_payload_buffer );
-			_payload_length = json_message.serialize_json(payload_buffer, TALKIE_BUFFER_SIZE);
-			return true;
+			
+			#ifdef BROADCAST_SPI_DEBUG
+				Serial.printf("\n[From Master] Slave: 0x%02X Beacon=0 L=%d\n", length, length);
+				Serial.printf("[To Slave] Sent %d bytes\n", length);
+				Serial.println(F("\t\t\t\t\tsend4: --> Broadcast sent to all pins -->"));
+			#endif
+
+			#ifdef BROADCAST_SPI_DEBUG_TIMING
+				Serial.print(" | ");
+				Serial.print(millis() - _reference_time);
+			#endif
 		}
         return false;
     }
 
 	
-    // Specific methods associated to ESP SPI as Slave
+    // Specific methods associated to ESP SPI as Master
+
+	bool broadcastLength(const int* ss_pins, uint8_t ss_pins_count, size_t length) {
+
+		if (length == 0 || length > TALKIE_BUFFER_SIZE) return false;
+
+		_tx_status_byte = (uint8_t)length;
+		spi_transaction_t t = {};
+		t.length = 1 * 8;	// Bytes to bits
+		t.tx_buffer = &_tx_status_byte;
+		t.rx_buffer = nullptr;
+
+		for (uint8_t ss_pin_i = 0; ss_pin_i < ss_pins_count; ss_pin_i++) {
+			digitalWrite(ss_pins[ss_pin_i], LOW);
+		}
+		spi_device_transmit(_spi, &t);
+		for (uint8_t ss_pin_i = 0; ss_pin_i < ss_pins_count; ss_pin_i++) {
+			digitalWrite(ss_pins[ss_pin_i], HIGH);
+		}
+		return true;
+	}
 	
-	void queue_status() {
-		_spi_state = WAIT_STATUS;
-		_tx_status_byte[_tx_status_index] = (uint8_t)_payload_length;
 
-		// Full-Duplex
-		spi_slave_transaction_t *t = &_data_trans;
-		memset(t, 0, sizeof(_data_trans));  // clear entire struct
-		t->length    = 1 * 8;
-		t->tx_buffer = &_tx_status_byte[_tx_status_index];
-		t->rx_buffer = &_rx_status_byte;
+	bool broadcastPayload(const int* ss_pins, uint8_t ss_pins_count, size_t length) {
 
-		spi_slave_queue_trans(_host, t, portMAX_DELAY);
+		if (length > TALKIE_BUFFER_SIZE) return false;
+
+		spi_transaction_t t = {};
+		t.length = length * 8;	// Bytes to bits
+		t.tx_buffer = _payload_buffer;
+		t.rx_buffer = nullptr;
+
+		for (uint8_t ss_pin_i = 0; ss_pin_i < ss_pins_count; ss_pin_i++) {
+			digitalWrite(ss_pins[ss_pin_i], LOW);
+		}
+		spi_device_transmit(_spi, &t);
+		for (uint8_t ss_pin_i = 0; ss_pin_i < ss_pins_count; ss_pin_i++) {
+			digitalWrite(ss_pins[ss_pin_i], HIGH);
+		}
+		// Border already included in the broadcast time slot
+		return true;
 	}
 
 
-	void queue_tx(size_t length = 0) {
-		_spi_state = TX_PAYLOAD;
-		// Half-Duplex
-		spi_slave_transaction_t *t = &_data_trans;
-		memset(t, 0, sizeof(_data_trans));  // clear entire struct
-		t->length    = length * 8;
-		t->tx_buffer = _tx_payload_buffer;
-		t->rx_buffer = nullptr;
+	size_t sendBeacon(int ss_pin) {
+
+		_tx_status_byte = 0;	// This marks it as a beacon
+		spi_transaction_t t = {};
+		t.length = 1 * 8;	// Bytes to bits
+		t.tx_buffer = &_tx_status_byte;
+		t.rx_buffer = &_rx_status_byte;
+
+		digitalWrite(ss_pin, LOW);
+		spi_device_transmit(_spi, &t);
+		digitalWrite(ss_pin, HIGH);
 		
-		spi_slave_queue_trans(_host, t, portMAX_DELAY);
+		if (_rx_status_byte > TALKIE_BUFFER_SIZE) return 0;
+		return (size_t)_rx_status_byte;
 	}
+	
 
-
-	void queue_rx(size_t length = 0) {
-		_spi_state = RX_PAYLOAD;
-		// Half-Duplex
-		spi_slave_transaction_t *t = &_data_trans;
-		memset(t, 0, sizeof(_data_trans));  // clear entire struct
-		t->length    = length * 8;
-		t->tx_buffer = nullptr;
-		t->rx_buffer = _rx_payload_buffer;
+	void receivePayload(int ss_pin, size_t length) {
+		spi_transaction_t t = {};
+		t.length = length * 8;	// Bytes to bits
+		t.tx_buffer = nullptr;
+		t.rx_buffer = _payload_buffer;
 		
-		spi_slave_queue_trans(_host, t, portMAX_DELAY);
+		digitalWrite(ss_pin, LOW);
+		spi_device_transmit(_spi, &t);
+		digitalWrite(ss_pin, HIGH);
 	}
 
 
 public:
 
     // Move ONLY the singleton instance method to subclass
-    static S_Broadcast_SPI_2xESP_Slave& instance(spi_host_device_t host = HSPI_HOST) {
-        static S_Broadcast_SPI_2xESP_Slave instance(host);
+    static S_Broadcast_SPI_2xESP_Master& instance(const int* ss_pins, uint8_t ss_pins_count, spi_host_device_t host = HSPI_HOST) {
+        static S_Broadcast_SPI_2xESP_Master instance(ss_pins, ss_pins_count, host);
 
         return instance;
     }
 
 
-    void begin(int mosi_io_num, int miso_io_num, int sclk_io_num, int spics_io_num) {
+    void begin(int mosi_io_num, int miso_io_num, int sclk_io_num) {
+		
+		// ================== CONFIGURE SS PINS ==================
+		// CRITICAL: Configure all SS pins as outputs and set HIGH
+		for (uint8_t ss_pin_i = 0; ss_pin_i < _ss_pins_count; ss_pin_i++) {
+			pinMode(_spi_cs_pins[ss_pin_i], OUTPUT);
+			digitalWrite(_spi_cs_pins[ss_pin_i], HIGH);
+		}
 
-		// https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/peripherals/spi_master.html
-		
-		// 1. ZERO the entire structs
 		spi_bus_config_t buscfg = {};
-		spi_slave_interface_config_t slvcfg = {};
-		
-		// 2. Set fields ONE BY ONE (no designator order issues!)
 		buscfg.mosi_io_num = mosi_io_num;
 		buscfg.miso_io_num = miso_io_num;
 		buscfg.sclk_io_num = sclk_io_num;
@@ -267,33 +268,39 @@ public:
 		buscfg.quadhd_io_num = -1;
 		buscfg.max_transfer_sz = TALKIE_BUFFER_SIZE;
 		
+		// https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/peripherals/spi_master.html
+
+		spi_device_interface_config_t devcfg = {};
+		devcfg.clock_speed_hz = 4000000;  // 4 MHz - Sweet spot!
+		devcfg.mode = 0;
+		devcfg.queue_size = 1;		// Only one queue is needed given that the payload is just 128 bytes
+		devcfg.spics_io_num = -1,  	// DISABLE hardware CS completely! (Broadcast)
+
+		spi_bus_initialize(_host, &buscfg, SPI_DMA_CH_AUTO);
+		spi_bus_add_device(_host, &devcfg, &_spi);
 		
-		// Newer ESP-IDF versions have these extra fields:
-		buscfg.data4_io_num = -1;
-		buscfg.data5_io_num = -1; 
-		buscfg.data6_io_num = -1;
-		buscfg.data7_io_num = -1;
-		
-		slvcfg.mode = 0;
-		slvcfg.spics_io_num = spics_io_num;
-		slvcfg.queue_size = 1;	// It's just 128 bytes maximum, so, a queue of 1 is all it needs
-		slvcfg.post_setup_cb = nullptr;
-		slvcfg.post_trans_cb = nullptr;
-    
-	
-		// DMA channel must be given if > 32 bytes
-		esp_err_t err = spi_slave_initialize(_host, &buscfg, &slvcfg, 1);  // ← CHANNEL 1
-		if (err == ESP_OK) {
-			queue_status();
-			_initiated = true;
-		}
+		_initiated = true;
 
 		#ifdef BROADCAST_SPI_DEBUG
-			Serial.println("Slave ready");
+		if (_initiated) {
+			Serial.print(class_description());
+			Serial.println(": initiate1: Socket initiated!");
+
+			Serial.print(F("\tinitiate2: Total SS pins connected: "));
+			Serial.println(_ss_pins_count);
+			Serial.print(F("\t\tinitiate3: SS pins: "));
+			
+			for (uint8_t ss_pin_i = 0; ss_pin_i < _ss_pins_count; ss_pin_i++) {
+				Serial.print(_spi_cs_pins[ss_pin_i]);
+				Serial.print(F(", "));
+			}
+			Serial.println();
+		} else {
+			Serial.println("initiate1: Socket NOT initiated!");
+		}
 		#endif
     }
-
 };
 
 
-#endif // BROADCAST_SPI_ESP2X_SLAVE_HPP
+#endif // BROADCAST_SPI_ESP2X_MASTER_HPP
