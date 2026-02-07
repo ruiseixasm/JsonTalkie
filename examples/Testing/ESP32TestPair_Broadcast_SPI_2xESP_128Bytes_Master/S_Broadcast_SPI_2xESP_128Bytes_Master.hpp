8ccf45e067835778a7f9803caeac202e5ac8cb21
Rui Monteiro <ruiseixas@yahoo.com>
2026-02-07 02:27:15 +0000

    Corrected moment of the _last_beacon_time_us setting

--- examples/Testing/ESP32TestPair_Broadcast_SPI_2xESP_128Bytes_Master/S_Broadcast_SPI_2xESP_128Bytes_Master.hpp
+++ examples/Testing/ESP32TestPair_Broadcast_SPI_2xESP_128Bytes_Master/S_Broadcast_SPI_2xESP_128Bytes_Master.hpp
@@ -82,7 +82,6 @@ protected:
 			
 			// Too many SPI sends to the Slaves asking if there is something to send will overload them, so, a timeout is needed
 			if (micros() - _last_beacon_time_us > beacon_time_slot_us) {
-				_last_beacon_time_us = micros();	// Avoid calling the beacon right away
 
 				#ifdef BROADCAST_SPI_DEBUG_TIMING
 				_reference_time = millis();
@@ -107,6 +106,7 @@ protected:
 					_startTransmission(new_message);
 				}
 				actual_pin_index = (actual_pin_index + 1) % _ss_pins_count;
+				_last_beacon_time_us = micros();	// Avoid calling the beacon right away
 			}
 		}
     }
