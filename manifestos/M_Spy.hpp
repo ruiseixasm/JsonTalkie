80e87b947c75ce6d15431446a4a22430277ec303
Rui Monteiro <ruiseixas@yahoo.com>
2026-02-11 01:08:40 +0000

    Added comments

--- manifestos/M_Spy.hpp
+++ manifestos/M_Spy.hpp
@@ -46,8 +46,8 @@ protected:
 	// The Action pair name and description shouldn't be greater than 40 chars
 	// {"m":7,"b":1,"i":6442,"f":"","t":"","0":255,"1":"","2":"","c":25870} <-- 128 - (68 + 2*10) = 40
 
-	// -------------- MAXIMUM SIZE RULER ---------------|
-	//	 "name", "123456789012345678901234567890"
+	// ------------- MAXIMUM SIZE RULER --------------|
+	//	 "name", "123456789012345678901234567890123456"
     Action calls[3] = {
 		{"ping", "Ping talkers by name or channel"},
 		{"ping_self", "I can even ping myself"},
