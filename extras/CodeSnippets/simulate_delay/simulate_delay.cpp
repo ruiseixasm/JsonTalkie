40f565859e6a750dbf9bb60e7dafe93d00e5d5d8
Rui Monteiro <ruiseixas@yahoo.com>
2026-02-12 23:00:34 +0000

    Correct output

--- extras/CodeSnippets/simulate_delay/simulate_delay.cpp
+++ extras/CodeSnippets/simulate_delay/simulate_delay.cpp
@@ -2,8 +2,8 @@
 
 int main() {
 
-	uint8_t _max_delay_ms = 5;
-	uint16_t _last_message_timestamp = 100;
+	const uint8_t _max_delay_ms = 5;
+	const uint16_t _last_message_timestamp = 100;
 
 	for (uint16_t message_timestamp = 105; message_timestamp > 90; --message_timestamp) {
 		const uint16_t remote_delay = _last_message_timestamp - message_timestamp;  // Package received after
@@ -12,6 +12,35 @@ int main() {
 			std::cout << "Message timestamp: " << message_timestamp << std::endl;
 		}
 	}
+
+	std::cout << std::endl;
+
+	// Works the same
+	for (uint16_t message_timestamp = 105; message_timestamp > 90; --message_timestamp) {
+		if (_last_message_timestamp - message_timestamp < 0xFFFF / 2
+			&& _last_message_timestamp - message_timestamp >= _max_delay_ms) {
+			
+			std::cout << "Message timestamp: " << message_timestamp << std::endl;
+		}
+	}
+
+	// OUTPUT:
+	// [Running] cd "/mnt/extra/GitHub/JsonTalkie/extras/CodeSnippets/simulate_delay/" && g++ simulate_delay.cpp -o simulate_delay && "/mnt/extra/GitHub/JsonTalkie/extras/CodeSnippets/simulate_delay/"simulate_delay
+	// Message timestamp: 95
+	// Message timestamp: 94
+	// Message timestamp: 93
+	// Message timestamp: 92
+	// Message timestamp: 91
+
+	// Message timestamp: 95
+	// Message timestamp: 94
+	// Message timestamp: 93
+	// Message timestamp: 92
+	// Message timestamp: 91
+
+	// [Done] exited with code=0 in 1.524 seconds
+
+
 
     return 0;
 }
