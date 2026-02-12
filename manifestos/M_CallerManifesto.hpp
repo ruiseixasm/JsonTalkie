f7be935d6434065e65fe209e7dfe9148cb79caa0
Rui Monteiro <ruiseixas@yahoo.com>
2026-02-12 15:31:23 +0000

    Reviewed CallerManifesto

--- manifestos/M_CallerManifesto.hpp
+++ manifestos/M_CallerManifesto.hpp
@@ -127,9 +127,10 @@ public:
 
 
 	void _loop(JsonTalker& talker) override {
-		uint32_t present_time = millis();
+		const uint32_t present_time = millis();
 		// 32 bits is 0xFFFFFFFF (4 bytes)
-		if ((int32_t)(present_time - _time_to_call) >= 0) {
+		const uint32_t call_overdue = present_time - _time_to_call;
+		if (call_overdue < 0xFFFFFFFF / 2) {
 			if (_active_caller) {
 				JsonMessage call_buzzer;
 				call_buzzer.set_message_value(MessageValue::TALKIE_MSG_CALL);
@@ -141,7 +142,8 @@ public:
 			// The time needs to be updated regardless of the transmission above
 			_time_to_call += 60UL * 60 * 1000;			// Add 60 minutes
 		}
-		if ((int32_t)(present_time - _time_to_live) >= 0) {
+		const uint32_t live_overdue = present_time - _time_to_live;
+		if (live_overdue < 0xFFFFFFFF / 2) {
 			digitalWrite(LED_BUILTIN, LOW);
 			_is_led_on = false;
 			_time_to_live = _time_to_call + 1UL * 60 * 1000;	// Add 1 minute extra
